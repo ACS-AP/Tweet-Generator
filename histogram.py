0172bf0284e87769118c59df25fe749d57b16b8f
ArnavPag <arnav.pagidyala@students.dominican.edu>
2022-01-24 17:55:18 -0800

    histogram

---
+++ histogram.py
@@ -0,0 +1,37 @@
+import sys
+
+def histogram(source_text):
+  dictionary = {}
+  for word in source_text:
+    if word in dictionary.keys():
+      dictionary[word] += 1 
+    else:
+      dictionary[word] = 1
+  return dictionary
+
+def unique_words(source_text):
+  dictionary = histogram(source_text)
+  print(str(len(dictionary)))
+
+def word_frequency(word, source_text):
+  dictionary = histogram(source_text)
+  if dictionary[word]:
+    if dictionary[word] > 1:
+      print("[" + str(dictionary[word]) + "] instances of " + str(word) +" unique words in the text.")
+    else: 
+      print("There is [" + str(dictionary[word]) + "] instances of " + str(word) +" unique words in the text.")
+  else:
+    print("Word was not found")
+
+if __name__ == '__main__':
+  file_name = sys.argv[1]
+  word_check = sys.argv[2]
+  f = open(file_name,'r', encoding='utf-8-sig')
+  blog_text = f.read()
+  clean_text = blog_text.replace(',','').replace('.','').replace('?','').replace("'",'').replace('"','').replace('-','')
+  source_text = clean_text.split(' ')
+  f.close()
+  histogram(source_text)
+  unique_words(source_text)
+  word_frequency(word_check, source_text)
+
